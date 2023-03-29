e30e8509808dbdfdb8af335b370e7c91f4928730
Mohamad Khajezade <khajezade.mohamad@gmail.com>
2023-03-29 12:01:35 -0700

    Data has been added

---
+++ selected_problems/cross_lingual_data_maker.py
@@ -0,0 +1,77 @@
+import os
+import json
+import pathlib
+
+
+current_location = pathlib.Path(__file__).parent.resolve()
+java = os.path.join(current_location, 'java_selected')
+ruby = os.path.join(current_location, 'ruby_selected')
+
+java_problems = os.listdir(java)
+data = []
+
+#Positive samples: 
+id_count = 0
+positive_count = 0
+negative_count = 0
+for p in java_problems: 
+    if '.' in p:
+        continue
+    
+    python_submissions = os.listdir(os.path.join(java, p))
+    ruby_submissions = os.listdir(os.path.join(ruby, p))
+    for i in range(3):
+        if positive_count >= 500:
+            break
+        for j in range(i+1, len(python_submissions)):
+            if j - i > 3:
+                break;
+            if positive_count >= 500:
+                break
+            
+            with open(os.path.join(java, p, python_submissions[i]), 'r') as f:
+                code1 = f.read()
+                
+            with open(os.path.join(ruby, p, ruby_submissions[j]), 'r') as f:
+                code2 = f.read()
+                
+            element = {'id': id_count, 'code1': code1, 'code2': code2,'label': 1}
+            data.append(element)
+            positive_count = positive_count + 1
+            id_count = id_count + 1
+            
+#negative samples
+java_problems = [p for p in java_problems if '.' not in p]
+for i in range(len(java_problems)):
+    if negative_count >= 500:
+        break
+    for j in range(i+1, len(java_problems)):
+        if negative_count >= 500:
+            break
+    
+        if j - i > 2:
+            break
+        
+        problem1 = java_problems[i]
+        problem2 = java_problems[j]
+        if problem1 == problem2:
+            continue
+        
+        p1_sub = os.listdir(os.path.join(java, problem1))
+        p2_sub = os.listdir(os.path.join(ruby, problem2))
+        
+        for index in range(3): 
+            with open(os.path.join(java, problem1, p1_sub[index]), 'r') as f:
+                code1 = f.read()
+                
+            with open(os.path.join(ruby, problem2, p2_sub[index]), 'r') as f:
+                code2 = f.read()
+                
+            element = {'id': id_count, 'code1': code1, 'code2': code2,'label': 0}
+            data.append(element)
+            id_count = id_count + 1
+            negative_count = negative_count + 1
+
+
+with open(os.path.join(current_location, 'ruby_java_test_clone.jsonl'),'w') as f:
+    f.write(json.dumps(data))
