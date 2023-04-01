69a9035bf59c8530d1532e5a9ec21e5f11983e55
Mohamad Khajezade <khajezade.mohamad@gmail.com>
2023-03-31 22:40:42 -0700

    computing the scores

--- selected_problems/analyser.py
+++ selected_problems/analyser.py
@@ -3,8 +3,31 @@ import re
 import json
 import pathlib
 
+from sklearn.metrics import precision_score, recall_score, f1_score
+
 
 current_location = pathlib.Path(__file__).parent.resolve()
+
+
+def get_labels(predictions, ground_truth):
+    ground_truth_labels = []
+    predictions_labels = []
+    for element in ground_truth:
+        key = list(element.keys())[0]
+        assert 1 == 1
+        if key in predictions:
+            ground_truth_labels.append(element[key])
+            predictions_labels.append(predictions[key])
+        
+        else:
+            assert 1==1
+        
+    return ground_truth_labels, predictions_labels
+
+
+def get_ground_trouth_lables(original_data):
+    samples_label = [{sample['id']: sample['label']} for sample in original_data]
+    return samples_label
 
 
 def read_data(data_file):
@@ -59,9 +82,28 @@ for key, value in java_processed_results.items():
         new_value = re.search(r"(0)+", value, re.IGNORECASE)
         if new_value:
             java_processed_results[key] = int(new_value.group(1))
-            assert 1 == 1
+            
+        else:
+            java_processed_results[key] = -1
+            
+            
+for key, value in java_ruby_processed_results.items():
+    if value not in [0, 1]:
+        java_ruby_processed_results[key] = -1
         
  
 original_java_data = read_data(os.path.join(current_location, 'java_test_clone.jsonl'))
 original_ruby_java_data = read_data(os.path.join(current_location, 'ruby_java_test_clone.jsonl'))
+java_ground_truth = get_ground_trouth_lables(original_java_data)
+ruby_java_ground_truth = get_ground_trouth_lables(original_ruby_java_data)
+java_prediction_ground_truth = get_labels(java_processed_results, java_ground_truth)
+java_ruby_prediction_ground_truth = get_labels(java_ruby_processed_results, ruby_java_ground_truth)
+assert 1 == 1
+precision_java = precision_score(java_prediction_ground_truth[0], java_prediction_ground_truth[1], average='macro')
+recall_java = recall_score(java_prediction_ground_truth[0], java_prediction_ground_truth[1], average='macro')
+f1_java = f1_score(java_prediction_ground_truth[0], java_prediction_ground_truth[1], average='macro')
+
+precision_java_ruby = precision_score(java_ruby_prediction_ground_truth[0], java_ruby_prediction_ground_truth[1], average='macro')
+recall_java_ruby = recall_score(java_ruby_prediction_ground_truth[0], java_ruby_prediction_ground_truth[1], average='macro')
+f1_java_ruby = f1_score(java_ruby_prediction_ground_truth[0], java_ruby_prediction_ground_truth[1], average='macro')
 assert 1 == 1
