f8936b16725440af0dc6cd98fd30f7bdacc20f02
Mohamad Khajezade <khajezade.mohamad@gmail.com>
2023-04-02 12:58:10 -0700

    Implementing classes for request and analysis

---
+++ selected_problems/analyse.py
@@ -0,0 +1,151 @@
+import os
+import re
+import json
+import pathlib
+
+from sklearn.metrics import precision_score, recall_score, f1_score
+
+
+current_location = pathlib.Path(__file__).parent.resolve()
+
+
+class Analyser:
+    """
+    A class to analyze and extract results from test and report files.
+    Attributes:
+        test_data (dict): A dictionary containing the test data.
+        report_file (str): The path to the report file.
+        results (dict): A dictionary containing the extracted results.
+    """
+
+    def __init__(self, test_file, report_file) -> None:
+        """
+        Initializes the Analyser class with the test and report files.
+
+        Args:
+            test_file (str): The path to the test data file.
+            report_file (str): The path to the report file.
+        """
+        self.precision = None
+        self.recall = None
+        self.f1_score = None
+        self.report_file = report_file
+        self.test_data = self.read_data(test_file)
+        self.results = self._extract_results()
+        
+    def read_data(self, data_file):
+        """
+        Reads data from the specified data file and returns it as a dictionary.
+
+        Args:
+            data_file (str): The path to the data file.
+
+        Returns:
+            dict: The data as a dictionary.
+        """
+        with open(data_file, 'r') as f:
+            for line in f:
+                data = json.loads(line)
+                
+        return data
+    
+    def _extract_results(self):
+        """
+        Extracts results from the report file and returns them as a dictionary.
+
+        Returns:
+            dict: The extracted results as a dictionary.
+        """
+        pure_results = self._read_lines(self.report_file)
+        result = {}
+        for data in pure_results:
+            try:
+                data_id = int(re.search(r"Data Id (\d+):", data).group(1))
+            except:
+                assert 1 == 1
+                
+            if 'yes' in data.lower():
+                data_value = 1
+            elif 'no' in data.lower():
+                data_value = 0
+            else:
+                assert 1 == 1
+            
+            result[data_id] = data_value
+            
+        return result
+
+    def _read_lines(self, file_path):
+        """
+        Reads lines from the specified file and returns a list of unprocessed results.
+
+        Args:
+            file_path (str): The path to the file.
+
+        Returns:
+            list: A list of unprocessed results.
+        """
+        unprocessed_results = []
+        with open(file_path, 'r') as f:
+            for line in f:
+                if '**' in line:
+                    block_begin = ''
+                
+                block_begin = block_begin + line.strip()
+                
+                if '++' in line:
+                    unprocessed_results.append(block_begin) 
+            
+        return unprocessed_results
+    
+    def _extract_ground_truth_labels(self):
+        samples_label = [{sample['id']: sample['label']} for sample in self.test_data]
+        return samples_label
+    
+    def compute_metrics(self):
+        ground_truth_labels = []
+        predictions_labels = []
+        
+        for element in self.ground_truth:
+            key = list(element.keys())[0]
+
+            if key in self.predicted_results:
+                ground_truth_labels.append(element[key])
+                predictions_labels.append(self.predicted_results[key])
+            else:
+                assert 1 == 1
+                
+        
+        self.precision = precision_score(ground_truth_labels, predictions_labels, average='macro')
+        self.recall = recall_score(ground_truth_labels, predictions_labels, average='macro')
+        self.f1_score = f1_score(ground_truth_labels, predictions_labels, average='macro') 
+        assert 1 == 1       
+        
+    @property
+    def predicted_results(self):
+        """
+        Returns the extracted results as a dictionary.
+
+        Returns:
+            dict: The extracted results as a dictionary.
+        """
+        return self.results
+    
+    @property
+    def ground_truth(self):
+        """
+        Returns the extracted results as a dictionary.
+
+        Returns:
+            dict: The extracted results as a dictionary.
+        """
+        return self._extract_ground_truth_labels()
+    
+
+if __name__ == '__main__':
+    analyser = Analyser(
+        os.path.join(current_location, 'java_test_clone_2.jsonl'),
+        os.path.join(current_location, 'results', 'results_java_01.txt')
+    )
+    ground_truth = analyser.compute_metrics()
+    assert analyser.predicted_results is not None
---
+++ selected_problems/chatgpt.py
@@ -0,0 +1,126 @@
+import os
+import json
+import pathlib
+
+import requests
+
+
+current_location = pathlib.Path(__file__).parent.resolve()
+
+
+class ChatGPTRequest:
+    def __init__(self, temperature=0.3, model='gpt-3.5-turbo'):
+        self.temperature = temperature
+        self.model = model
+        self.results = []
+        self.api_key = self._read_api_key()
+
+    def _read_api_key(self):
+        with open("/Users/mohamadkhajezade/workspace/openai.txt", "r") as file:
+            return file.readline().strip()
+
+    def _send_request(self, prompt_id, prompt):
+        url = "https://api.openai.com/v1/chat/completions"
+        headers = {
+            "Content-Type": "application/json",
+            "Authorization": f"Bearer {self.api_key}",
+        }
+        data = {
+            "model": self.model,
+            "messages": [{"role": "user", "content": f"{prompt}"}],
+            "temperature": self.temperature
+        }
+        response = requests.post(url, headers=headers, json=data)
+        print(f"The request status for prompt id {prompt_id} is {response.status_code}\n")
+        return response.json()
+
+    def process_prompts(self, prompts, requested_samples_file, output_file):
+        requested_ids = self._get_requested_ids(requested_samples_file)
+        for prompt in prompts:
+            sample_id = prompt[0]
+            if sample_id in requested_ids:
+                continue
+            
+            with open(requested_samples_file, 'a') as file:
+                file.write(f"{sample_id}\n")
+                
+            result = self._send_request(prompt[0], prompt[1])
+            if not 'choices' in result:
+                continue
+            
+            with open(output_file, 'a') as file:
+                file.write(f"***Data Id {sample_id}: {result['choices'][0]['message']['content'].strip()}+++\n \n")
+                
+    def _get_requested_ids(self, requested_sample_file):
+        requested_ids = []
+        if not os.path.exists(requested_sample_file):
+            return []
+        with open(requested_sample_file, 'r') as file:
+            for line in file:
+                requested_ids.append(int(line.strip()))
+            
+        return requested_ids
+        
+                
+               
+class CodeCloneDetection:
+    def __init__(self, data_file, temperature=0.3, model='gpt-3.5-turbo' ) -> None:
+        self.model = model
+        self.temperature = temperature
+        self.data = self._read_data(data_file)
+        self.prompts = [self._make_probmpt(d['id'], d['code1'], d['code2']) for d in self.data]
+        self.gpt = ChatGPTRequest(temperature=self.temperature, model=self.model)
+    
+    def _get_requested_ids(self, file_name):
+        requested_ids = []
+        if not os.path.exists(file_name):
+            return []
+        with open(file_name, 'r') as file:
+            for line in file:
+                requested_ids.append(int(line.strip()))
+            
+        return requested_ids
+
+    def _read_data(self, data_file):
+        with open(data_file, 'r') as f:
+            for line in f:
+                data = json.loads(line)
+                
+        return data
+
+    def _make_probmpt(self, id, code1, code2):
+        prompt = f"""
+        code1:
+        {code1}
+        code2:
+        {code2}
+        do code 1 and code 2 solve identical problems with the same inputs and outputs? answer with yes or no and no explanation. 
+        """
+        return (id, prompt)
+    
+    def run_processing(self, requested_samples_file, output_file):
+        self.gpt.process_prompts(self.prompts, requested_samples_file, output_file)
+        return self
+
+
+
+
+def main():
+    # chat_gpt = ChatGPTRequest()
+    # data = read_data(os.path.join(current_location, 'java_test_clone_2.jsonl'))
+    
+    
+    # chat_gpt.process_prompts(prompts)
+    ccd01 = CodeCloneDetection(
+        os.path.join(current_location, 'java_test_clone_2.jsonl'),
+        temperature=0.1
+    )
+    ccd01.run_processing(
+        os.path.join(current_location, 'results', 'requested_ids_0.1.txt'), 
+        os.path.join(current_location, 'results', 'results_java_01.txt')
+    )
+    assert 1 == 1
+
+if __name__ == "__main__":
+    main()
+
